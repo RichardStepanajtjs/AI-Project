b2f01ff7da7ef82368757f5c86083463769c8fe3
Richard <r.stepanajtjs@gmail.com>
2026-04-27 12:01:29 +0200

    Updated API url to lessen clutter

--- Frontend/src/app/services/prospectslist/prospectslist-service.ts
+++ Frontend/src/app/services/prospectslist/prospectslist-service.ts
@@ -6,29 +6,29 @@ import { inject, Injectable } from '@angular/core';
 })
 export class ProspectslistService {
   private http = inject(HttpClient);
-  private apiUrl = 'https://nest.sokrates.traefik.me';
+  private apiUrl = 'https://nest.sokrates.traefik.me/prospects';
 
   getProspectsByTag(tag: string) {
-    return this.http.get(`${this.apiUrl}/prospects/filter/${tag}`);
+    return this.http.get(`${this.apiUrl}/filter/${tag}`);
   }
   
   createProspect(data: any) {
-    return this.http.post(`${this.apiUrl}/prospects`, data);
+    return this.http.post(`${this.apiUrl}`, data);
   }
 
   getProspects() {
-    return this.http.get(`${this.apiUrl}/prospects`);
+    return this.http.get(`${this.apiUrl}`);
   }
 
   getProspectById(id: string) {
-    return this.http.get(`${this.apiUrl}/prospects/${id}`);
+    return this.http.get(`${this.apiUrl}/${id}`);
   }
 
   updateProspect(id: string, data: any) {
-    return this.http.put(`${this.apiUrl}/prospects/${id}`, data);
+    return this.http.put(`${this.apiUrl}/${id}`, data);
   }
 
   deleteProspect(id: string) {
-    return this.http.delete(`${this.apiUrl}/prospects/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
